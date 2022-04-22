01501a2a2b40e1287c5cad90db4179441da3bbfd
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-04-22 23:21:04 +0200

    Functional! Yey!

--- script.js
+++ script.js
@@ -2,45 +2,27 @@ let activeIdiom
 activeIdiom = 'en'
 
 const textContents = {}
+textContents.line0 = {}
 textContents.line1 = {}
 textContents.line2 = {}
-textContents.line3 = {}
 
-textContents.line1.en = 'Hello, World!'
-textContents.line1.fr = 'Bonjour, Monde!'
-textContents.line1.pt = 'Olá, Mundo!'
+textContents.line0.en = 'Hello, World!'
+textContents.line0.fr = 'Bonjour, Monde!'
+textContents.line0.pt = 'Olá, Mundo!'
 
-textContents.line2.en = 'With 509 votes to 3 and 47 abstentions, the European Parliament adopted on Thursday a resolution recalling various measures necessary to protect children and young people fleeing violence and facilitate their integration into host country communities.'
-textContents.line2.fr = 'Par 509 voix pour, 3 contre et 47 abstentions, le Parlement a adopté jeudi une résolution rappelant diverses mesures nécessaires pour protéger les enfants et les jeunes fuyant la violence et pour faciliter leur intégration dans les communautés des pays d’accueil.'
-textContents.line2.pt = 'Com 509 votos a favor, 3 contra e 47 abstenções, o Parlamento Europeu adotou na quinta-feira uma resolução que aponta várias medidas necessárias para proteger as crianças e os jovens que fogem da violência e facilitar a sua integração nas comunidades dos países de acolhimento.'
+textContents.line1.en = 'With 509 votes to 3 and 47 abstentions, the European Parliament adopted on Thursday a resolution recalling various measures necessary to protect children and young people fleeing violence and facilitate their integration into host country communities.'
+textContents.line1.fr = 'Par 509 voix pour, 3 contre et 47 abstentions, le Parlement a adopté jeudi une résolution rappelant diverses mesures nécessaires pour protéger les enfants et les jeunes fuyant la violence et pour faciliter leur intégration dans les communautés des pays d’accueil.'
+textContents.line1.pt = 'Com 509 votos a favor, 3 contra e 47 abstenções, o Parlamento Europeu adotou na quinta-feira uma resolução que aponta várias medidas necessárias para proteger as crianças e os jovens que fogem da violência e facilitar a sua integração nas comunidades dos países de acolhimento.'
 
-textContents.line3.en = '“Every child has the right to be protected from violence, exploitation and abuse”, say MEPs, calling on EU countries to protect children from the risk of trafficking, illegal adoption and other types of abuse.'
-textContents.line3.fr = '“Tous les enfants ont le droit d’être protégés contre la violence, l’exploitation et les abus”, affirment les députés, qui appellent les pays de l’UE à les protéger contre les risques de traite, d’adoption illégale et d’autres types d’abus.'
-textContents.line3.pt = '“Todas as crianças têm o direito de ser protegidas contra a violência, exploração e abusos”, defendem os eurodeputados, apelando aos Estados-Membros para que protejam as crianças do risco de tráfico, adoção ilegal e outros tipos de abuso.'
+textContents.line2.en = '“Every child has the right to be protected from violence, exploitation and abuse”, say MEPs, calling on EU countries to protect children from the risk of trafficking, illegal adoption and other types of abuse.'
+textContents.line2.fr = '“Tous les enfants ont le droit d’être protégés contre la violence, l’exploitation et les abus”, affirment les députés, qui appellent les pays de l’UE à les protéger contre les risques de traite, d’adoption illégale et d’autres types d’abus.'
+textContents.line2.pt = '“Todas as crianças têm o direito de ser protegidas contra a violência, exploração e abusos”, defendem os eurodeputados, apelando aos Estados-Membros para que protejam as crianças do risco de tráfico, adoção ilegal e outros tipos de abuso.'
 
-// //initial text insert
-// function setIdiom(idiom) {
-//   document.querySelectorAll('.changeable-text').forEach(span => {
-//     span.innerHTML = (textContents[span.id])[idiom]
-//   })  
-// }
-// setIdiom(activeIdiom)
-
-
-// function changeIdiom(newIdiom='en') {
-  
-  //   document.querySelectorAll('.changeable-text').forEach(span => {
-    
-    //   })
-    //   activeIdiom = newIdiom
-    // }
-    
-function textChanger(newString, spanId) {
+function textChanger(newString, span) {
   return new Promise((resolve, reject) => {
-    const span = document.querySelector(spanId)
-    const oldString = span.innerText
-    
+        
     //create arrays from strings
+    const oldString = span.innerText
     const newStringArray = []
     const oldStringArray = []
     for (let i = 0; i < newString.length; i++) {
@@ -75,8 +57,7 @@ function textChanger(newString, spanId) {
           change()        
         } else {
           span.innerText = newString
-          resolve()
-          console.log('resolved!')
+          resolve()          
         }
       }, 1) //set interval
     }  
@@ -84,22 +65,10 @@ function textChanger(newString, spanId) {
   })
 }
 
-async function changeIdiom(idiom) {
-  await textChanger(textContents.line1[idiom], '#line1')
-  await textChanger(textContents.line2[idiom], '#line2')
-  await textChanger(textContents.line3[idiom], '#line3')
-  return 'done!'
+
+
+async function changeIdiom(idiom) {  
+  for (let i = 0; i < document.querySelectorAll('.changeable-text').length; i++) {
+    await textChanger((textContents[`line${i}`])[idiom], document.querySelector(`#line${i}`))
+  }
 }
-
-// textChanger(textContents.line2.fr, '#line2')
-setTimeout(() => {
-  changeIdiom('pt')  
-}, 2000);
-
-setTimeout(() => {
-  changeIdiom('fr')  
-}, 6000);
-
-setTimeout(() => {
-  changeIdiom('en')  
-}, 10000);
