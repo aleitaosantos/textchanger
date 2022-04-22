d1e0f1b52ddb9aa44dab3f30a650ff292a0611de
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-04-22 18:01:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,64 +18,72 @@ textContents.line3.en = '“Every child has the right to be protected from viole
 textContents.line3.fr = '“Tous les enfants ont le droit d’être protégés contre la violence, l’exploitation et les abus”, affirment les députés, qui appellent les pays de l’UE à les protéger contre les risques de traite, d’adoption illégale et d’autres types d’abus.'
 textContents.line3.pt = '“Todas as crianças têm o direito de ser protegidas contra a violência, exploração e abusos”, defendem os eurodeputados, apelando aos Estados-Membros para que protejam as crianças do risco de tráfico, adoção ilegal e outros tipos de abuso.'
 
-//initial text insert
-function setIdiom(idiom) {
-  document.querySelectorAll('.changeable-text').forEach(span => {
-    span.innerHTML = (textContents[span.id])[idiom]
-  })  
-}
-setIdiom(activeIdiom)
+// //initial text insert
+// function setIdiom(idiom) {
+//   document.querySelectorAll('.changeable-text').forEach(span => {
+//     span.innerHTML = (textContents[span.id])[idiom]
+//   })  
+// }
+// setIdiom(activeIdiom)
 
 
-function changeIdiom(newIdiom='en') {
-  let spanId, string, newString, oldString
-  let newStringArray = [], oldStringArray = []
-  let changedString
+// function changeIdiom(newIdiom='en') {
   
-  document.querySelectorAll('.changeable-text').forEach(span => {
-    spanId = span.id
-    string = textContents[spanId]
-    newString = string[newIdiom]
-    oldString = string[activeIdiom]
+  //   document.querySelectorAll('.changeable-text').forEach(span => {
     
-    //create arrays from strings
-    newStringArray = []
-    oldStringArray = []
-    for (let i = 0; i < newString.length; i++) {
-      newStringArray.push(newString[i])
-    }
-    for (let i = 0; i < oldString.length; i++) {
-      oldStringArray.push(oldString[i])
-    }
+    //   })
+    //   activeIdiom = newIdiom
+    // }
+    
+function textChanger(newString, spanId) {
+  
+  const span = document.querySelector(spanId)
+  const oldString = span.innerHTML
 
-    //equalize string arrays
-    if (newString.length < oldString.length) {
-      for (let i = 0; i <= oldStringArray.length - newStringArray.length; i++) {
-        newStringArray.push(null)    
-      }    
+  const newStringArray = []
+  const oldStringArray = []
+  let changedString
+    
+  //create arrays from strings
+
+  for (let i = 0; i < newString.length; i++) {
+    newStringArray.push(newString[i])
+  }
+  for (let i = 0; i < oldString.length; i++) {
+    oldStringArray.push(oldString[i])
+  }
+
+  //equalize string arrays
+  if (newString.length < oldString.length) {
+    for (let i = 0; i <= oldStringArray.length - newStringArray.length; i++) {
+      newStringArray.push(null)    
+    }    
+  }
+  else if (newStringArray.length > oldStringArray.length) {
+    for (let i = 0; i <= newStringArray.length - oldStringArray.length; i++) {
+      oldStringArray.push(null)      
     }
-    else if (newStringArray.length > oldStringArray.length) {
-      for (let i = 0; i <= newStringArray.length - oldStringArray.length; i++) {
-        oldStringArray.push(null)      
+  }
+
+  //substitute each character at time
+  changedStringArray = oldStringArray
+  
+  let i = 0 
+  function myLoop() {         
+    setTimeout(function() {   
+      changedStringArray[i] = newStringArray[i]
+      changedString = changedStringArray.join('')
+      span.innerHTML = changedString
+      i++
+      if (i < changedStringArray.length) {
+        myLoop()
       }
-    }
+    }, 10) //set interval
+  }  
+  myLoop()
 
-    //substitute each character at time
-    changedStringArray = oldStringArray
-    
-    let i = 0 
-    function myLoop() {         
-      setTimeout(function() {   
-        changedStringArray[i] = newStringArray[i]
-        changedString = changedStringArray.join('')
-        document.querySelector(`#${spanId}`).innerHTML = changedString
-        i++
-        if (i < changedStringArray.length) {
-          myLoop()
-        }
-      }, 10) //set interval
-    }  
-    myLoop()
-  })
-  activeIdiom = newIdiom
 }
+
+
+
+textChanger('Bonjour, Monde!', '#line1')
