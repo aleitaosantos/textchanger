28d2cdebcc019ad82b7b8d151f973c3fb6aea439
Alexandre Leitão Santos <alexandre.leitao.santos@gmail.com>
2022-04-23 01:01:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-let activeIdiom
-activeIdiom = 'en'
-
-const textContents = {}
-textContents.line0 = {}
-textContents.line1 = {}
-textContents.line2 = {}
-
-textContents.line0.en = 'Hello, World!'
-textContents.line0.fr = 'Bonjour, Monde!'
-textContents.line0.pt = 'Olá, Mundo!'
-
-textContents.line1.en = 'With 509 votes to 3 and 47 abstentions, the European Parliament adopted on Thursday a resolution recalling various measures necessary to protect children and young people fleeing violence and facilitate their integration into host country communities.'
-textContents.line1.fr = 'Par 509 voix pour, 3 contre et 47 abstentions, le Parlement a adopté jeudi une résolution rappelant diverses mesures nécessaires pour protéger les enfants et les jeunes fuyant la violence et pour faciliter leur intégration dans les communautés des pays d’accueil.'
-textContents.line1.pt = 'Com 509 votos a favor, 3 contra e 47 abstenções, o Parlamento Europeu adotou na quinta-feira uma resolução que aponta várias medidas necessárias para proteger as crianças e os jovens que fogem da violência e facilitar a sua integração nas comunidades dos países de acolhimento.'
-
-textContents.line2.en = '“Every child has the right to be protected from violence, exploitation and abuse”, say MEPs, calling on EU countries to protect children from the risk of trafficking, illegal adoption and other types of abuse.'
-textContents.line2.fr = '“Tous les enfants ont le droit d’être protégés contre la violence, l’exploitation et les abus”, affirment les députés, qui appellent les pays de l’UE à les protéger contre les risques de traite, d’adoption illégale et d’autres types d’abus.'
-textContents.line2.pt = '“Todas as crianças têm o direito de ser protegidas contra a violência, exploração e abusos”, defendem os eurodeputados, apelando aos Estados-Membros para que protejam as crianças do risco de tráfico, adoção ilegal e outros tipos de abuso.'
-
-function textChanger(newString, span) {
+function textChanger(newString, span, interval) {
   return new Promise((resolve, reject) => {
         
-    //create arrays from strings
+    // create arrays from strings
     const oldString = span.innerText
     const newStringArray = []
     const oldStringArray = []
@@ -31,10 +11,10 @@ function textChanger(newString, span) {
     for (let i = 0; i < oldString.length; i++) {
       oldStringArray.push(oldString[i])
     }
-    newStringArrayLength = newStringArray.length
-    oldStringArrayLength = oldStringArray.length
+    let newStringArrayLength = newStringArray.length
+    let oldStringArrayLength = oldStringArray.length
 
-    //equalize string arrays
+    // equalize string arrays lenght
     if (newStringArrayLength < oldStringArrayLength) {
       for (let i = 0; i < oldStringArrayLength - newStringArrayLength; i++) {
         newStringArray.push(null)    
@@ -45,28 +25,29 @@ function textChanger(newString, span) {
         oldStringArray.push(null)      
       }
     }
-    changedStringArray = oldStringArray
-    i = 0 
+    
+    // change one character at time
+    let changedStringArray = oldStringArray
+    let i = 0 
     function change() {         
-      setTimeout(function() {   
+      setTimeout(() => {   
         changedStringArray[i] = newStringArray[i]
-        changedString = changedStringArray.join('')
+        if (i < changedStringArray.length-1) {
+          changedStringArray[i+1] = '\u25AE'
+        }
+        let changedString = changedStringArray.join('')
         span.innerText = changedString
         i++
-        if (i < changedStringArray.length) {
+        if (i < changedStringArray.length) {          
           change()        
         } else {
           span.innerText = newString
           resolve()          
         }
-      }, 1) //set interval
+      }, interval)
     }  
     change()    
   })
 }
 
-async function changeIdiom(idiom) {  
-  for (let i = 0; i < document.querySelectorAll('.changeable-text').length; i++) {
-    await textChanger((textContents[`line${i}`])[idiom], document.querySelector(`#line${i}`))
-  }
-}
+export { textChanger }
